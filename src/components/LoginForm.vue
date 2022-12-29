960ad15df857b2b669415107405b5f72e79c8b47
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-29 11:04:11 +0300

    clear

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,13 +1,30 @@
 <template>
-    <div>
-
-    </div>
+    <form @submit.prevent="uyeOl">
+        <h4>Giriş Ekranı</h4>
+        <input type="text" placeholder="E-Mail Giriniz" v-model="email">
+        <input type="password" placeholder="Parola Giriniz" v-model="parola">
+        <button>Giriş</button>
+    </form>
 </template>
 
-<script>
-    export default {
-        
+<script >
+import { ref } from 'vue'
+
+
+export default {
+    setup() {
+        const email = ref('')
+        const parola = ref('')
+
+        const uyeOl = () => {
+            console.log(kullaniciAd.value, email.value, parola.value);
+
+        }
+        return { email, parola, uyeOl }
+
     }
+}
+
 </script>
 
 <style scoped>
