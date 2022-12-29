c17003e1631afae0840106de87cb784a5321d1e1
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-29 10:25:01 +0300

    firestore settings

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFVqNoH3zchuEHkAbdqdVpgQLuheBE-CQ",
+
+  authDomain: "blog-app-7cc8a.firebaseapp.com",
+
+  databaseURL: "https://blog-app-7cc8a-default-rtdb.firebaseio.com",
+
+  projectId: "blog-app-7cc8a",
+
+  storageBucket: "blog-app-7cc8a.appspot.com",
+
+  messagingSenderId: "291121184274",
+
+  appId: "1:291121184274:web:25a9b801728438ecd122cb",
+
+  measurementId: "G-THQ5DM78F7",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const tarih = firebase.firestore.FieldValue.serverTimestamp;
