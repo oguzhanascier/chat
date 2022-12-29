4189619e5711ed7014288ddc792404b173361e3e
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-29 11:39:34 +0300

    created sinup and useRegister func

--- src/compasables/useRegister.js
+++ src/compasables/useRegister.js
@@ -2,3 +2,13 @@ import {ref} from 'vue'
 import {auth} from '../firebase/config'
 
 const hata= ref(null)
+
+const signup= async (email, parolai, kullaniciAd)=>{
+
+}
+
+
+const useRegister=()=>{
+
+return {hata,signup}
+}
