a1425cff7165532eff9548f54d76bb467ad3ca88
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-30 12:06:33 +0300

    chatoda router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ChatOda from '../views/ChatOda.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },  {
+    path: '/chat',
+    name: 'chat',
+    component: ChatOda
   },
 
 ]
