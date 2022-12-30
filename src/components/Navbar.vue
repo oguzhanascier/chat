6d66f57d257771cd834c8bb638b13396f1ac89e8
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-30 12:21:32 +0300

    created

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Kullanıcı Adı</p>
+        <p>Kullanıcı Emaili</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
