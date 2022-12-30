4fe6a5f23aefec361f769ca5f7439efd94d120f8
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-30 15:22:22 +0300

    :::::::::))

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,8 +1,8 @@
 <template>
     <nav>
         <div>
-            <p>Kullanıcı Adı</p>
-            <p class="email">Kullanıcı Emaili</p>
+            <p>Merhaba {{kullanici.displayName}}</p>
+            <p class="email">{{kullanici.email}} ile giriş yaptınız.</p>
         </div>
         <button @click="cikisYap">çıkış</button>
 
@@ -15,12 +15,13 @@ import getUser from '../compasables/getUser'
 export default {
     setup() {
         const router = useRouter()
+        const { kullanici } = getUser()
         const cikisYap = async () => {
             await auth.signOut().then(() => {
                 router.push({ name: 'home' })
             })
         }
-        return {cikisYap}
+        return { cikisYap, kullanici }
     }
 }
 </script>
