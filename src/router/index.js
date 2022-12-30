ebf59f775a8e85a4aae9ecbc67e3f0a7630a9069
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-30 15:22:16 +0300

    routerguard

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ChatOda from '../views/ChatOda.vue'
+import { auth } from '@/firebase/config'
+
+
+
+const authControl = (to, from, next) => {
+  const kullanici = auth.currentUser
+
+  if (!kullanici) {
+    next({ name: 'home' })
+  }else{
+    next()
+  }
+
+}
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },  {
+  }, {
     path: '/chat',
     name: 'chat',
-    component: ChatOda
+    component: ChatOda,
+    beforeEnter: authControl
   },
 
 ]
