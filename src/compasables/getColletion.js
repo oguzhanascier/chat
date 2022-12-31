de6686c4103c033b9871f195fbf0ce1e95a3f468
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-31 12:14:55 +0300

    mistake collettion property name

--- src/compasables/getColletion.js
+++ src/compasables/getColletion.js
@@ -5,7 +5,7 @@ const getColletion = (koleksiyon) => {
     const belgeler = ref(null)
     const hata = ref(null)
 
-    let koleksiyonRef = db.collection(koleksiyon).orderBy('tarih')
+    let koleksiyonRef = db.collection(koleksiyon).orderBy('tarihi')
     koleksiyonRef.onSnapshot(snap => {
         let sonuclar = []
         snap.docs.forEach(doc => {
