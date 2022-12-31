8756839596aa52d87720b87e6edf62c1afab7924
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-31 12:04:50 +0300

    export getColletion

---
+++ src/compasables/getColletion.js
@@ -0,0 +1,23 @@
+import { ref } from "vue";
+import { db } from '../firebase/config'
+
+const getColletion = (koleksiyon) => {
+    const belgeler = ref(null)
+    const hata = ref(null)
+
+    let koleksiyonRef = db.collection(koleksiyon).orderBy('tarih')
+    koleksiyonRef.onSnapshot(snap => {
+        let sonuclar = []
+        snap.docs.forEach(doc => {
+            doc.data().tarih && sonuclar.push({ ...doc.data, id: doc.id })
+        })
+        belgeler.value = sonuclar
+        hata.value = null
+    }, err => {
+        belgeler.value = null
+        hata.value = 'server error'
+    })
+    return { hata, belgeler }
+}
+
+export default getColletion
