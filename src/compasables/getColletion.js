b8ae294341f9246baed9a86f028adf0f3ebb06ff
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-31 12:39:04 +0300

    :(

--- src/compasables/getColletion.js
+++ src/compasables/getColletion.js
@@ -5,17 +5,20 @@ const getColletion = (koleksiyon) => {
     const belgeler = ref(null)
     const hata = ref(null)
 
-    let koleksiyonRef = db.collection(koleksiyon).orderBy('tarihi')
+    let koleksiyonRef = db.collection(koleksiyon).orderBy('tarih')
+    console.log(koleksiyon);
     koleksiyonRef.onSnapshot(snap => {
         let sonuclar = []
         snap.docs.forEach(doc => {
             doc.data().tarih && sonuclar.push({ ...doc.data, id: doc.id })
         })
         belgeler.value = sonuclar
+        console.log(belgeler);
         hata.value = null
     }, err => {
         belgeler.value = null
         hata.value = 'server error'
+        console.log(err);
     })
     return { hata, belgeler }
 }
